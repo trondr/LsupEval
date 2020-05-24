efadb5086e194ca75a76d4243b371534294b7850
trondr <trondr@outlook.com>
2020-05-24 19:12:08 +0200

    Add support for logging

---
+++ src/lib/LsupEval/Logging.fs
@@ -0,0 +1,13 @@
+﻿
+namespace LsupEval
+
+module internal Logging=
+    
+    open Common.Logging
+
+    let getLoggerByName (name:string) = 
+        LogManager.GetLogger(name)
+
+    type Msg = System.Action<Common.Logging.FormatMessageHandler>
+    let msg message =
+        new Msg(fun m -> m.Invoke(message)|>ignore)
