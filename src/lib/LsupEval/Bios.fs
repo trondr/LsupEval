5fcea616ed08e340fd71e69da30712e22a11fa47
trondr <trondr@outlook.com>
2020-05-25 00:23:58 +0200

    Code refactoring. Implement WmiHelper

--- src/lib/LsupEval/Bios.fs
+++ src/lib/LsupEval/Bios.fs
@@ -3,6 +3,7 @@
 module Bios =
     open System.Management
     open LsupEval.Logging
+    open LsupEval.WmiHelper
     let logger = getLoggerByName "Bios"
 
     type Bios = {Versions:string[]}
@@ -12,20 +13,12 @@ module Bios =
         new System.Text.RegularExpressions.Regex(pattern)
 
     let getCurrentBiosVersion () =
-        try
-            use searcher = new ManagementObjectSearcher("SELECT SMBIOSBIOSVersion FROM WIN32_BIOS")
-            use list = searcher.Get()
-            let biosVersion = 
-                list
-                |>Seq.cast
-                |>Seq.map(fun (x:ManagementObject) -> x.["SMBIOSBIOSVersion"])
-                |>Seq.head
-            Result.Ok (biosVersion :?> string)
-        with
-        |ex -> 
-            Result.Error (toException ("Failed to get BIOS version.") (Some ex))
+        result{
+            let! objectValue = getWmiPropertyValue "Win32_BIOS" "SMBIOSBIOSVersion"
+            return  objectValue :?> string
+        }
 
-    let isBiosMatch (logger:Common.Logging.ILog) (bios:Bios) biosVersion =        
+    let isBiosMatch (logger:Common.Logging.ILog) (bios:Bios) biosVersion =
         let v = 
             bios.Versions 
             |> Seq.filter (fun s -> 
---
+++ src/lib/LsupEval/WmiHelper.fs
@@ -0,0 +1,18 @@
+﻿namespace LsupEval
+
+module WmiHelper =
+    open System.Management
+
+    let getWmiPropertyValue className propertyName =
+        try
+            use searcher = new ManagementObjectSearcher(sprintf "SELECT %s FROM %s" propertyName className)
+            use list = searcher.Get()
+            let propertyValue = 
+                list
+                |>Seq.cast
+                |>Seq.map(fun (x:ManagementObject) -> x.[propertyName])
+                |>Seq.head
+            Result.Ok propertyValue
+        with
+        |ex -> 
+            Result.Error (toException (sprintf "Failed to get property value '%s' from class '%s'." propertyName className) (Some ex))
